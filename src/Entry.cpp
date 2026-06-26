2231f746bcaad2052d866cdc75964f9bf1a3bf70
MisledWater079 <59891258+Misledwater79@users.noreply.github.com>
2026-06-26 00:07:53 -0400

    Fix plugin name

--- src/Entry.cpp
+++ src/Entry.cpp
@@ -1,6 +1,6 @@
 #include "AntiXray.hpp"
 
-ENDSTONE_PLUGIN("endstone_antixray", "1.0.0", AntiXray) {
+ENDSTONE_PLUGIN("antixray", "1.0.0", AntiXray) {
     authors = { "MisledWater79" };
     description = "AntiXray plugin for Endstone";
     prefix = "mw";
